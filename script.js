713ebcbdeaa931e3097174d0e6ae74368550086e
Nikita Taranuh <103592404+KennyKom@users.noreply.github.com>
2024-05-05 20:51:23 +0300

    add Bar Chart settings

---
+++ script.js
@@ -0,0 +1,27 @@
+export const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+export const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+export const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments';
+
+import generateBarChart, { canvasBarChart } from './charts/barChart.js';
+
+const bodyContent = document.querySelector('.page');
+
+export default async function getData(url) {
+  const data = await fetch(url);
+
+  if (!data.ok) {
+    throw new Error(`Server is unavailable`);
+  }
+
+  return data.json();
+}
+
+setTimeout(function () {
+  bodyContent.classList.add('page_visible');
+}, 0);
+
+setTimeout(function () {
+    canvasBarChart.classList.add('canvas__visible');
+}, 0);
+  
+generateBarChart();
